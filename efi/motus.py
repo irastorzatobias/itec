65b58fa2093c1bce234d76e0a150c307dd6f5afe
Tobias <tobiasirastorza@gmail.com>
2021-10-23 02:26:07 -0300

    styling

--- efi/motus.py
+++ efi/motus.py
@@ -6,17 +6,21 @@ sg.theme('DarkBrown4')
 people = [{
     'nombre': 'Tobias Irastorza',
     'turnos': ['Hoy', 'Mañana', 'Pasado'],
-    'cuota': '29/08/2021'
+    'pago': '29/08/2021',
+    'cuota':False
 },
 {        
     'nombre': 'Lautaro Irastorza',
     'turnos': ['Lunes', 'Miercoles', 'Viernes'],
-    'cuota': '29/08/2021'
+    'pago': '29/08/2021',
+    'cuota':False
+    
 },
 {        
     'nombre': 'Lautaro Martinez',
     'turnos': ['Lunes', 'Jueves','Viernes'],
-    'cuota': '29/08/2021'
+    'pago': '29/08/2021', 
+    'cuota':False  
 },
 ]
 routines = ['Dia 1', 'Dia 2', 'Dia 3', 'Dia 4', 'Dia 5'] 
@@ -25,7 +29,7 @@ peoples_name = [x['nombre'] for x in people]
 
 # Funciones utiles no relacionadas al LAYOUT
 def distance_between_dates(date1, date2):
-    """ Return distance between dates in days"""
+    """ Retorna distancia entre dos fechas en dias"""
     return abs(date2 - date1).days
 
 def filter_by_turn(dict, day):
@@ -34,22 +38,23 @@ def filter_by_turn(dict, day):
 
 
 def get_day_name(date):
-    """ Retorna el nombre del dia """
+    """ Retorna el nombre del dia pasandole una fecha como parametro """
     day_name = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes','Sabado','Domingo']
     return day_name[date.weekday()]
 
-def hide_unhide(actual, previous):
-    """ Esconde la ventana actual y muestra la ventana anterior pasada como"""
-    previous.un_hide()
+def hide_unhide(actual, target):
+    """ Esconde la ventana actual y muestra la ventana pasada como target"""
+    target.un_hide()
     actual.hide()
     
 def ver_turno(persona):
     """ Printea los turnos de las personas """
     text = ''
     for k,v in persona.items():
+        # Exceptuo los items cuota y pago
         if k in 'turnos':
             text += f'{v}\n'
-        else:
+        elif k in 'nombre':
             text += f'{v} -- '
     return text
 
@@ -64,31 +69,31 @@ def get_alumno_index(name):
 def principal():
     """ Layout principal """
     columna = [
-                [sg.Text('MOTUS',font=('Arial',40,BOLD),pad=(0,(0,25)))],
-                [sg.B('TURNOS', size=(20,1), pad=(0,(0,25)))],
-                [sg.B('RUTINAS', size=(20,1), pad=(0,(0,25)))],
-                [sg.B('ALUMNOS', size=(20,1), pad=(0,(0,25)))],
-                [sg.B('GESTION CUOTAS', size=(20,1), pad=(0,(0,25)))],
-                [sg.B('SALIR', size=(20,1), pad=(0,(0,25)))],
+                [sg.Text('MOTUS',font=('webhostinghub-glyphs',40,'bold'),pad=(0,(0,25)))],
+                [sg.B('TURNOS', size=(15,2), font=('webhostinghub-glyphs',10,'bold'), pad=(0,(0,25)))],
+                [sg.B('ALUMNOS', size=(15,2), font=('webhostinghub-glyphs',10,'bold'), pad=(0,(0,25)))],
+                [sg.B('RUTINAS', size=(15,2), font=('webhostinghub-glyphs',10,'bold'), pad=(0,(0,25)))],
+                [sg.B('GESTION CUOTAS', size=(15,2), font=('webhostinghub-glyphs',10,'bold'), pad=(0,(0,25)))],
+                [sg.B('SALIR', size=(15,2), font=('webhostinghub-glyphs',10,'bold'),pad=(0,(0,25)))],
                 ]
     layout = [
                 [sg.Column(columna,element_justification='c',pad=(0,100))],
                 ]
-    return sg.Window('MOTUS TRAINING', layout,size=(800,600),finalize=True, element_justification='c')
+    return sg.Window('MOTUS TRAINING', layout,size=(800,600),finalize=True, element_justification='c',button_color=('#c93c36'))
 
 # Layout de los turnos
 def turnos():
     columna = [
-                [sg.Text('TURNERO',font=('Arial',40,BOLD), pad=(0,(0,25)))],
-                [sg.B('Gestionar turnos', size=(20,1), pad=(0,(0,25)))],
-                [sg.B('Turnos del dia', size=(20,1), pad=(0,(0,25)))],
-                [sg.B('Ver turnos', size=(20,1), pad=(0,(0,25)))],
-                [sg.B('Volver', size=(20,1), pad=(0,(0,25)))],
+                [sg.Text('TURNERO',font=('Courier New',40,'bold'), pad=(0,(0,25)))],
+                [sg.B('Gestionar turnos', size=(15,2), font=('webhostinghub-glyphs',10,'bold'), pad=(0,(0,25)))],
+                [sg.B('Turnos del dia', size=(15,2), font=('webhostinghub-glyphs',10,'bold'), pad=(0,(0,25)))],
+                [sg.B('Ver turnos', size=(15,2), font=('webhostinghub-glyphs',10,'bold'), pad=(0,(0,25)))],
+                [sg.B('Volver', size=(15,2), font=('webhostinghub-glyphs',10,'bold'), pad=(0,(0,25)))],
                 ]
     layout = [
         [sg.Column(columna, element_justification='c', pad=(0,100))]
     ]
-    return sg.Window('TURNOS',layout,size=(800,600),finalize=True, element_justification='c' )
+    return sg.Window('TURNOS',layout,size=(800,600),finalize=True, element_justification='c',button_color=('#c93c36'))
 
 def add_turno():
     columna = [
@@ -105,9 +110,9 @@ def add_turno():
     layout = [
         [sg.Text('AGREGANDO TURNO', font=('Arial',30))],
         [sg.Column(columna2,element_justification='c'),sg.Column(columna, element_justification='c', pad=((50,0),100))],
-        [sg.B('Actualizar', size=(20,1)),sg.B('Volver',size=(20,1))]
+        [sg.B('Actualizar', size=(15,2), font=('webhostinghub-glyphs',10,'bold')),sg.B('Volver',size=(15,2))]
         ]
-    return sg.Window('Agregar turno', layout, size=(800,600), finalize=True, element_justification='c')
+    return sg.Window('Agregar turno', layout, size=(800,600), finalize=True, element_justification='c',button_color=('#c93c36'))
 
 
 # Layout de los alumnos 
@@ -115,13 +120,13 @@ def alumnos():
     columna = [
                 [sg.Text('ALUMNOS',font=('Arial',40), pad=(0,(0,25)))],
                 [sg.Listbox(peoples_name, size=(20,20), key='alumno', pad=(0,(0,25)))],
-                [sg.B('Agregar'), sg.B('Modificar'), sg.B('Borrar'),],
-                [sg.B('Ver', size=(7,1)),sg.B('Volver', size=(7,1))]
+                [sg.B('Agregar', font=('webhostinghub-glyphs',10,'bold')), sg.B('Modificar',font=('webhostinghub-glyphs',10,'bold')), sg.B('Borrar',font=('webhostinghub-glyphs',10,'bold')),],
+                [sg.B('Ver', size=(7,1), font=('webhostinghub-glyphs',10,'bold'),),sg.B('Volver', size=(7,1), font=('webhostinghub-glyphs',10,'bold'),)]
                 ]
     layout = [
         [sg.Column(columna, element_justification='c')]
     ]
-    return sg.Window('TURNOS',layout,size=(800,600),finalize=True, element_justification='c')
+    return sg.Window('TURNOS',layout,size=(800,600),finalize=True, element_justification='c',button_color=('#c93c36'))
     
 
 # En un principio es para modificar a los alumnos
@@ -133,23 +138,23 @@ def rutinas():
     columna = [
                 [sg.Text('RUTINAS',font=('Arial',40))],
                 [sg.Listbox(routines, size=(20,len(routines)), key='rutina', pad=(0,(0,25)))],
-                [sg.B('Agregar'), sg.B('Modificar'), sg.B('Borrar')],
-                [sg.B('Ver', size=(7,1)),sg.B('Volver', size=(7,1))]
+                [sg.B('Agregar', font=('webhostinghub-glyphs',10,'bold')), sg.B('Modificar',font=('webhostinghub-glyphs',10,'bold')), sg.B('Borrar',font=('webhostinghub-glyphs',10,'bold'))],
+                [sg.B('Ver', size=(7,1), font=('webhostinghub-glyphs',10,'bold')),sg.B('Volver',font=('webhostinghub-glyphs',10,'bold'), size=(7,1))]
                 ]
     layout = [
         [sg.Column(columna, element_justification='c', pad=(0,150))]
     ]
-    return sg.Window('RUTINAS',layout,size=(800,600),finalize=True, element_justification='c')
+    return sg.Window('RUTINAS',layout,size=(800,600),finalize=True, element_justification='c',button_color=('#c93c36'))
     
 def display_routine(value):
     columna = [
                 [sg.Text(f'RUTINA {value["rutina"][0]}',font=('Arial',40))],
-                [sg.B('Volver', size=(7,1))]
+                [sg.B('Volver', font=('webhostinghub-glyphs',10,'bold'),size=(7,1))]
                 ]
     layout = [
         [sg.Column(columna, element_justification='c', pad=(0,150))]
     ]
-    return sg.Window('RUTINAS',layout,size=(800,600),finalize=True, element_justification='c')
+    return sg.Window('RUTINAS',layout,size=(800,600),finalize=True, element_justification='c',button_color=('#c93c36'))
     
 
 
@@ -185,21 +190,21 @@ def main():
             days = [k.title() for k,v in values.items() if v == True] # dias seleccionados
             print(days)
             if len(days) == 0 or values['alumno'] == []:
-                sg.popup('Seleccione al menos un dia / alumno')
+                sg.popup('Seleccione al menos un dia / alumno', font=(20))
             else:
                 people[pos].update({'turnos':days})
-                sg.popup('Turnos actualizados')
-        if event == 'Turnos del dia':
+                sg.popup('Turnos actualizados', font=(20))
+        if event == 'Turnos del dia' and window == turnos_layout:
             # Chequeando turnos del dia
             dia = get_day_name(datetime.today())
             day_turns = filter_by_turn(people, dia) 
             if len(day_turns) == 0:
-                sg.popup(f'No hay turnos para el dia {dia}')
+                sg.popup(f'No hay turnos para el dia {dia}',font=(20))
             else:
                 texto = f'Personas que asisten el dia de la fecha:\n'
                 for p in day_turns:
                     texto += f'{p["nombre"]}\n'
-                sg.popup(texto)            
+                sg.popup(texto,font=(20))
         if event == 'Volver' and window == add_turno_layout:
             # Volviendo desde agregar turnos, al layout de turnos
             hide_unhide(add_turno_layout, turnos_layout)
@@ -208,7 +213,7 @@ def main():
             texto = ''
             for p in people:
                 texto += ver_turno(p)
-            sg.popup(texto)
+            sg.popup(texto, font=(20))
         # Alumnos
         if event == 'ALUMNOS' and window == principal_layout:
             alumnos_layout = alumnos()
@@ -232,5 +237,5 @@ def main():
                 
 
 if __name__ == '__main__':
-    # main()
-    print(distance_between_dates(datetime.today(), datetime.strptime(people[0]['cuota'],'%d/%m/%Y')))
+    main()
+    # print(distance_between_dates(datetime.today(), datetime.strptime(people[0]['cuota'],'%d/%m/%Y')))
